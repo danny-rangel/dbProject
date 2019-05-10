c7df483692937f80c7f09cb5958cb8bc0017c05c
Danny Rangel <danielrangel.tech@gmail.com>
2019-05-10 01:57:11 -0700

    fixed column name bug and styled tables some more

--- ResultsProvider.php
+++ ResultsProvider.php
@@ -10,11 +10,11 @@ class ResultsProvider {
 
 
     public function getProfessorCoursesHtml($ssn) {
-        $query = $this->con->prepare("SELECT Title, Classroom, Meeting_Days, Start_Time, End_Time 
-                                        FROM Course_Section
-                                        NATURAL JOIN Taught_By
-                                        NATURAL JOIN Course
-                                        WHERE Social_Security_Number=:ssn");
+        $query = $this->con->prepare("SELECT title, classroom, meeting_days, start_time, end_time 
+                                        FROM course_sections
+                                        NATURAL JOIN taught_by
+                                        NATURAL JOIN courses
+                                        WHERE social_security_number=:ssn");
 
         $query->bindParam(":ssn", $ssn);
         $query->execute();
@@ -23,27 +23,27 @@ class ResultsProvider {
 
             $resultsHtml = "<table class='dataTable'>
                             <tr>
-                                <td class='dataLabel'>Title</td>
-                                <td class='dataLabel'>Room</td>
-                                <td class='dataLabel'>Meeting Days</td>
-                                <td class='dataLabel'>Start Time</td>
-                                <td class='dataLabel'>End Time</td>
+                                <td class='dataMainLabel'>Title</td>
+                                <td class='dataMainLabel'>Room</td>
+                                <td class='dataMainLabel'>Meeting Days</td>
+                                <td class='dataMainLabel'>Start Time</td>
+                                <td class='dataMainLabel'>End Time</td>
                             </tr>";
 
         while($row = $query->fetch(PDO::FETCH_ASSOC)) {
 
-            $Title = $row["Title"];
-            $Classroom = $row["Classroom"];
-            $Meeting_Days = $row["Meeting_Days"];
-            $Start_Time = $row["Start_Time"];
-            $End_Time = $row["End_Time"];
+            $title = $row["title"];
+            $classroom = $row["classroom"];
+            $meeting_days = $row["meeting_days"];
+            $start_time = $row["start_time"];
+            $end_time = $row["end_time"];
 
             $resultsHtml .= "<tr>
-                                <td>$Title</td>
-                                <td>$Classroom</td>
-                                <td>$Meeting_Days</td>
-                                <td>$Start_Time</td>
-                                <td>$End_Time</td>
+                                <td class='dataLabel'>$title</td>
+                                <td class='dataLabel'>$classroom</td>
+                                <td class='dataLabel'>$meeting_days</td>
+                                <td class='dataLabel'>$start_time</td>
+                                <td class='dataLabel'>$end_time</td>
                             </tr>";
         }
 
@@ -62,11 +62,11 @@ class ResultsProvider {
 
 
         public function getProfessorStudentGradesHtml($courseNum, $sectionNum) {
-            $query = $this->con->prepare("SELECT Grade, count(distinct Grade) 
-                                            FROM Enrollment_Record
-                                            WHERE Course_No=:courseNum
-                                            AND Section_No=:sectionNum
-                                            GROUP by Grade");
+            $query = $this->con->prepare("SELECT grade, count(distinct grade) 
+                                            FROM enrollment_records
+                                            WHERE course_number=:courseNum
+                                            AND section_number=:sectionNum
+                                            GROUP by grade");
     
             $query->bindParam(":courseNum", $courseNum);
             $query->bindParam(":sectionNum", $sectionNum);
@@ -77,18 +77,18 @@ class ResultsProvider {
 
                 $resultsHtml = "<table class='dataTable'>
                                 <tr>
-                                    <td class='dataLabel'>Grade</td>
-                                    <td class='dataLabel'>Count</td>
+                                    <td class='dataMainLabel'>Grade</td>
+                                    <td class='dataMainLabel'>Count</td>
                                 </tr>";
 
                 while($row = $query->fetch(PDO::FETCH_ASSOC)) {
     
-                    $Grade = $row["Grade"];
-                    $count = $row["count(distinct Grade)"];
+                    $grade = $row["grade"];
+                    $count = $row["count(distinct grade)"];
         
                     $resultsHtml .= "<tr>
-                                        <td>$Grade</td>
-                                        <td>$count</td>
+                                        <td class='dataLabel'>$grade</td>
+                                        <td class='dataLabel'>$count</td>
                                     </tr>";
                 }
 
@@ -107,11 +107,11 @@ class ResultsProvider {
 
 
         public function getCourseSectionsHtml($courseNum) {
-            $query = $this->con->prepare("SELECT Section_No, Classroom, Meeting_Days, Start_Time, End_Time, count(distinct CWID) 
-                                            FROM Course_Section
-                                            NATURAL JOIN Enrollment_Record
-                                            WHERE Course_No=:courseNum
-                                            GROUP BY Section_No");
+            $query = $this->con->prepare("SELECT section_number, classroom, meeting_days, start_time, end_time, count(distinct cwid) 
+                                            FROM course_sections
+                                            NATURAL JOIN enrollment_records
+                                            WHERE course_number=:courseNum
+                                            GROUP BY section_number");
     
             $query->bindParam(":courseNum", $courseNum);
             $query->execute();
@@ -120,30 +120,30 @@ class ResultsProvider {
     
                 $resultsHtml = "<table class='dataTable'>
                                 <tr>
-                                    <td class='dataLabel'>Section No.</td>
-                                    <td class='dataLabel'>Room</td>
-                                    <td class='dataLabel'>Meeting Days</td>
-                                    <td class='dataLabel'>Start Time</td>
-                                    <td class='dataLabel'>End Time</td>
-                                    <td class='dataLabel'># of Enrolled Students</td>
+                                    <td class='dataMainLabel'>Section No.</td>
+                                    <td class='dataMainLabel'>Room</td>
+                                    <td class='dataMainLabel'>Meeting Days</td>
+                                    <td class='dataMainLabel'>Start Time</td>
+                                    <td class='dataMainLabel'>End Time</td>
+                                    <td class='dataMainLabel'># of Enrolled Students</td>
                                 </tr>";
     
             while($row = $query->fetch(PDO::FETCH_ASSOC)) {
     
-                $Section_No = $row["Section_No"];
-                $Classroom = $row["Classroom"];
-                $Meeting_Days = $row["Meeting_Days"];
-                $Start_Time = $row["Start_Time"];
-                $End_Time = $row["End_Time"];
-                $count = $row["count(distinct CWID)"];
+                $section_number = $row["section_number"];
+                $classroom = $row["classroom"];
+                $meeting_days = $row["meeting_days"];
+                $start_time = $row["start_time"];
+                $end_time = $row["end_time"];
+                $count = $row["count(distinct cwid)"];
     
                 $resultsHtml .= "<tr>
-                                    <td>$Section_No</td>
-                                    <td>$Classroom</td>
-                                    <td>$Meeting_Days</td>
-                                    <td>$Start_Time</td>
-                                    <td>$End_Time</td>
-                                    <td>$count</td>
+                                    <td class='dataLabel'>$section_number</td>
+                                    <td class='dataLabel'>$classroom</td>
+                                    <td class='dataLabel'>$meeting_days</td>
+                                    <td class='dataLabel'>$start_time</td>
+                                    <td class='dataLabel'>$end_time</td>
+                                    <td class='dataLabel'>$count</td>
                                 </tr>";
             }
     
@@ -168,10 +168,10 @@ class ResultsProvider {
 
 
         public function getCourseHistoryHtml($cwid) {
-            $query = $this->con->prepare("SELECT Section_No, Course_No, Title, Grade 
-                                            FROM Enrollment_Record
-                                            NATURAL JOIN Course
-                                            WHERE CWID=:cwid");
+            $query = $this->con->prepare("SELECT section_number, course_number, title, grade 
+                                            FROM enrollment_records
+                                            NATURAL JOIN courses
+                                            WHERE cwid=:cwid");
     
             $query->bindParam(":cwid", $cwid);
             $query->execute();
@@ -180,24 +180,24 @@ class ResultsProvider {
     
                 $resultsHtml = "<table class='dataTable'>
                                 <tr>
-                                    <td class='dataLabel'>Section No.</td>
-                                    <td class='dataLabel'>Course No.</td>
-                                    <td class='dataLabel'>Title</td>
-                                    <td class='dataLabel'>Grade</td>
+                                    <td class='dataMainLabel'>Section No.</td>
+                                    <td class='dataMainLabel'>Course No.</td>
+                                    <td class='dataMainLabel'>Title</td>
+                                    <td class='dataMainLabel'>Grade</td>
                                 </tr>";
     
             while($row = $query->fetch(PDO::FETCH_ASSOC)) {
     
-                $Section_No = $row["Section_No"];
-                $Course_No = $row["Course_No"];
-                $Title = $row["Title"];
-                $Grade = $row["Grade"];
+                $section_number = $row["section_number"];
+                $course_number = $row["course_number"];
+                $title = $row["title"];
+                $grade = $row["grade"];
     
                 $resultsHtml .= "<tr>
-                                    <td>$Section_No</td>
-                                    <td>$Course_No</td>
-                                    <td>$Title</td>
-                                    <td>$Grade</td>
+                                    <td class='dataLabel'>$section_number</td>
+                                    <td class='dataLabel'>$course_number</td>
+                                    <td class='dataLabel'>$title</td>
+                                    <td class='dataLabel'>$grade</td>
                                 </tr>";
             }
     
@@ -212,14 +212,6 @@ class ResultsProvider {
                 return $resultsHtml;
             }
         }
-
-
-
-
-
-
-
-
 
 }
 
